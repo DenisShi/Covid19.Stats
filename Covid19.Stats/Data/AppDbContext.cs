557c195a63150e137d372b2c8e45673f02892809
denis <okposhka09@gmail.com>
2022-01-13 22:44:40 +0700

    Контекст данных

---
+++ Covid19.Stats/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Covid19.Stats.Data
+
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CovidStat> Stats { get; set; }
+    }
+}
