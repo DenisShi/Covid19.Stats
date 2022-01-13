6900b1dcf1ee47cfea74e16d6efa33a600d7e3e1
denis <okposhka09@gmail.com>
2022-01-14 01:07:07 +0700

    Получение глобальнйо статистики

--- Covid19.Stats/Services/StatsService.cs
+++ Covid19.Stats/Services/StatsService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Covid19.Stats.Data;
+using Covid19.Stats.Models;
 
 
 namespace Covid19.Stats.Services
@@ -14,6 +15,17 @@ namespace Covid19.Stats.Services
         public StatsService(AppDbContext context)
         {
             _context = context;
+        }
+
+        public GlobalStatSummaryViewModel GetGlobalStat()
+        {
+            var lastData = _context.Stats
+                .Where(s => s.DateTime == _context.Stats.Max(x => x.DateTime));
+            var CasesSum = lastData.Sum(x => x.Confirmed);
+            var DeathSum = lastData.Sum(x => x.Deaths);
+
+            return new GlobalStatSummaryViewModel() { Cases = CasesSum, Deaths = DeathSum };
+                
         }
     }
 }
