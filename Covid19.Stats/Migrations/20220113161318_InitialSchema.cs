eb09d6c613d4274b695067391290309bf81edb20
denis <okposhka09@gmail.com>
2022-01-13 23:14:07 +0700

    Создана первая миграция базы данных

---
+++ Covid19.Stats/Migrations/20220113161318_InitialSchema.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Covid19.Stats.Migrations
+{
+    public partial class InitialSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProvinceState = table.Column<string>(type: "TEXT", nullable: true),
+                    CountryRegion = table.Column<string>(type: "TEXT", nullable: true),
+                    Confirmed = table.Column<int>(type: "INTEGER", nullable: false),
+                    Deaths = table.Column<int>(type: "INTEGER", nullable: false),
+                    Recovered = table.Column<int>(type: "INTEGER", nullable: false),
+                    Active = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stats", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stats");
+        }
+    }
+}
