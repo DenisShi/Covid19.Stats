c03bf0d9437ee4105b1bd1bd63216b14b92384ab
denis <okposhka09@gmail.com>
2022-01-13 22:05:36 +0700

    Добавил модель основной таблицы

---
+++ Covid19.Stats/Model/CovidStat.cs
@@ -0,0 +1,12 @@
+﻿namespace Covid19.Stats.Model
+{
+    public class CovidStat
+    {
+        public string ProvinceState { get; set; }
+        public string CountryRegion { get; set; }
+        public int Confirmed { get; set; }
+        public int Deaths { get; set; }
+        public int Recovered { get; set; }
+        public int Active { get; set; }
+    }
+}
