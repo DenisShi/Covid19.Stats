ec9e41b7349bb871b31d3b93d4865fe523984afd
denis <okposhka09@gmail.com>
2022-01-14 00:19:13 +0700

    Добавлена VM данных для главной страницы

---
+++ Covid19.Stats/Models/StatSummaryViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Covid19.Stats.Data;
+
+namespace Covid19.Stats.Models
+{
+    public class StatSummaryViewModel
+    {
+        public int Id { get; set; }
+        public string ProvinceState { get; set; }
+        public string CountryRegion { get; set; }
+        public int Confirmed { get; set; }
+        public int Deaths { get; set; }
+        public string Date { get; set; }
+        public static StatSummaryViewModel FromStat(CovidStat stat)
+        {
+            return new StatSummaryViewModel
+            {
+                Id = stat.Id,
+                ProvinceState = stat.ProvinceState,
+                CountryRegion = stat.CountryRegion,
+                Confirmed = stat.Confirmed,
+                Deaths = stat.Deaths,
+                Date = $"{stat.DateTime.Day}.{stat.DateTime.Month}.{stat.DateTime.Year}"
+            };
+        }
+    }
+}
