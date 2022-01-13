ca6dfc2edcd44024755684d6d49b6c92e748a0fe
denis <okposhka09@gmail.com>
2022-01-14 01:27:59 +0700

    CountrySummaryViewModel

---
+++ Covid19.Stats/Models/CountrySummaryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid19.Stats.Models
+{
+    public class CountrySummaryViewModel
+    {
+        public string Country { get; set; }
+        public int Confirmed { get; set; }
+        public int Deaths { get; set; }
+    }
+}
