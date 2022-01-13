fd4ee362482f7294b9ed25b74b46bcdec4dc6ebb
denis <okposhka09@gmail.com>
2022-01-14 01:06:07 +0700

    GlobalStatVM

---
+++ Covid19.Stats/Models/GlobalStatSummaryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Covid19.Stats.Data;
+
+namespace Covid19.Stats.Models
+{
+    public class GlobalStatSummaryViewModel
+    {
+         public int Cases { get; set; }
+         public int Deaths { get; set; }
+    }
+}
