360dca42a22ce435ae37c9ea9431af646fb78ea8
denis <okposhka09@gmail.com>
2022-01-14 00:06:45 +0700

    Добавлен сервис статистики

---
+++ Covid19.Stats/Services/StatsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Covid19.Stats.Data;
+
+
+namespace Covid19.Stats.Services
+{
+    public class StatsService
+    {
+        readonly AppDbContext _context;
+        public StatsService(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
