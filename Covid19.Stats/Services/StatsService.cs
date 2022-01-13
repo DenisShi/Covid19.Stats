f324c5222284076f3c0c51b2c6434ea8c457b8c1
denis <okposhka09@gmail.com>
2022-01-14 03:44:55 +0700

    Получение информации по стране

--- Covid19.Stats/Services/StatsService.cs
+++ Covid19.Stats/Services/StatsService.cs
@@ -38,6 +38,20 @@ namespace Covid19.Stats.Services
                     Deaths = x.Sum(y => y.Deaths),
                 });
         }
+        public CountrySummaryViewModel GetCountryStat(string country)
+        {
+            return getLastData()
+                .Where(x => x.CountryRegion == country)
+                .GroupBy(
+                x => x.CountryRegion,
+                x => new { x.Confirmed, x.Deaths })
+                .Select(x => new CountrySummaryViewModel
+                {
+                    Country = x.Key,
+                    Confirmed = x.Sum(y => y.Confirmed),
+                    Deaths = x.Sum(y => y.Deaths),
+                }).FirstOrDefault();
+        }
         private IQueryable<CovidStat> getLastData()
         {
             return _context.Stats
