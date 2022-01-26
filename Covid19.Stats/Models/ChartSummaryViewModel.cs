74283fc0cff31fab2143c2633657829ad3fe0d17
denis <okposhka09@gmail.com>
2022-01-26 19:09:10 +0700

    chart VM

---
+++ Covid19.Stats/Models/ChartSummaryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid19.Stats.Models
+{
+    public class ChartSummaryViewModel
+    {
+        int Cases { get; set; }
+        int Deaths { get; set; }
+        DateTime Date { get; set; }
+    }
+}
