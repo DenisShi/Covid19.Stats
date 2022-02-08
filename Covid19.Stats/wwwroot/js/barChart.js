df94bbdb6bafc3164ce98612cc7a9f9eee839ebf
denis <okposhka09@gmail.com>
2022-02-08 20:56:53 +0700

    barChart function

---
+++ Covid19.Stats/wwwroot/js/barChart.js
@@ -0,0 +1,26 @@
+﻿function BuildBarChart(labels, cases, canvasId, chartName) {
+    this.labels = labels;
+    const defaultColor = 'rgb(0, 154, 207)'
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: chartName,
+            backgroundColor: defaultColor,
+            borderColor: defaultColor,
+            data: cases, }]
+    };
+
+
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {}
+    };
+
+    const ch = new Chart(
+        document.getElementById(canvasId),
+        config
+    );
+
+}
