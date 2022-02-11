6dd0d3039e2d5c81fc8a9e26fa1ef799d9ad8f5d
denis <okposhka09@gmail.com>
2022-02-11 19:25:20 +0700

    geoChart

---
+++ Covid19.Stats/Models/Extensions/CountrySummaryCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Covid19.Stats.Models;
+namespace Covid19
+{
+    public static class CountrySummaryCollectionExtension
+    {
+        public static Dictionary<string, int> GetMapInfo(this IEnumerable<CountrySummaryViewModel> countrySummaries)
+        {
+            Dictionary<string, int> pairs = new();
+            foreach (var cs in countrySummaries)
+            {
+                pairs.Add(cs.Country, cs.Cases);
+            }
+            return pairs;
+        }
+    }
+}
---
+++ Covid19.Stats/wwwroot/js/geoChart.js
@@ -0,0 +1,27 @@
+﻿function BuildGeoChart(json_header, json_data, canvas_id) {
+    var result = [];
+    result.push(json_header)
+    for (var i in json_data)
+        result.push([i, json_data[i]]);
+
+
+
+
+    google.charts.load('current', {
+        'packages': ['geochart'],
+    });
+    google.charts.setOnLoadCallback(drawRegionsMap);
+
+    function drawRegionsMap() {
+        var data = google.visualization.arrayToDataTable(result);
+
+        var options = {
+            colorAxis: { colors: ['red'] },
+
+        };
+
+        var chart = new google.visualization.GeoChart(document.getElementById(canvas_id));
+
+        chart.draw(data, options);
+    }
+}
