a0ece730509faa87103f4c23b22cf5d360b882c6
denis <okposhka09@gmail.com>
2022-02-17 11:38:57 +0700

    Date migration

--- Covid19.Stats/Migrations/20220217043340_AddEfDate.cs
+++ Covid19.Stats/Migrations/20220217043340_AddEfDate.cs
@@ -3,12 +3,19 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Covid19.Stats.Migrations
 {
-    public partial class DataTimeField : Migration
+    public partial class AddEfDate : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.AddColumn<DateTime>(
-                name: "DateTime",
+                name: "Date2",
+                table: "Stats",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Last_Update2",
                 table: "Stats",
                 type: "TEXT",
                 nullable: false,
@@ -18,7 +25,11 @@ namespace Covid19.Stats.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropColumn(
-                name: "DateTime",
+                name: "Date2",
+                table: "Stats");
+
+            migrationBuilder.DropColumn(
+                name: "Last_Update2",
                 table: "Stats");
         }
     }
