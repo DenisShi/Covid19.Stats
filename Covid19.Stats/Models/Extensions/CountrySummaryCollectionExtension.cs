2f3ec79a2ab7b704d6aca6f04065134890998d6e
denis <okposhka09@gmail.com>
2022-02-17 15:09:33 +0700

    Карта смертности

--- Covid19.Stats/Models/Extensions/CountrySummaryCollectionExtension.cs
+++ Covid19.Stats/Models/Extensions/CountrySummaryCollectionExtension.cs
@@ -7,12 +7,22 @@ namespace Covid19
 {
     public static class CountrySummaryCollectionExtension
     {
-        public static Dictionary<string, int> GetMapInfo(this IEnumerable<CountrySummaryViewModel> countrySummaries)
+        public static Dictionary<string, int> GetCasesMapInfo(this IEnumerable<CountrySummaryViewModel> countrySummaries)
         {
             Dictionary<string, int> pairs = new();
             foreach (var cs in countrySummaries)
             {
                 pairs.Add(cs.Country, cs.Cases);
+            }
+            return pairs;
+        }
+
+        public static Dictionary<string, int> GetDeathsMapInfo(this IEnumerable<CountrySummaryViewModel> countrySummaries)
+        {
+            Dictionary<string, int> pairs = new();
+            foreach (var cs in countrySummaries)
+            {
+                pairs.Add(cs.Country, cs.Deaths);
             }
             return pairs;
         }
